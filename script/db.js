07c40c7c3e62224e4d54b229fa6fdd7e38c701ef
Andrew <andre.gutierrez9190@gmail.com>
2026-04-07 11:09:27 -0300

    modificaciones dia 07/04

--- script/db.js
+++ script/db.js
@@ -1,21 +1,24 @@
 export const Productos = [
     {
         ID:1,
-        Nombre:"Laptop",
+        Nombre:"Notebook",
         Precio: 250000,
         Stock: 10,
-        Descripcion: "Una laptop potente para todas tus necesidades"
+        Descripcion: "Una notebook potente para todas tus necesidades",
+        image:"../src/productos/notebook.png"
      },{
         ID:2,
         Nombre:"Computadora de escritorio",
         Precio: 500000,
         Stock: 10,
-        Descripcion: "Una computadora de escritorio potente para todas tus necesidades"
+        Descripcion: "Una computadora de escritorio potente para todas tus necesidades",
+        image:"../src/productos/computadora.png"
      },{
         ID:3,
         Nombre:"celular",
         Precio: 150000,
         Stock: 10,
-        Descripcion: "Un celular potente para todas tus necesidades"
+        Descripcion: "Un celular potente para todas tus necesidades",
+        image:"../src/productos/celular.png"
      }
 ]
--- script/dom.js
+++ script/dom.js
@@ -6,10 +6,11 @@ Productos.forEach(i => {
     let tarjeta = document.createElement("div");
     tarjeta.classList.add("cards");
     tarjeta.innerHTML = `
+            <img src="${i.image}" alt="${i.Nombre}" height="300px">
             <h1>${i.Nombre}</h1>
-            <p>${i.Descripcion}</p>
             <p>Precio: $${i.Precio}</p>
             <p>Stock: ${i.Stock}</p>
+            <p>${i.Descripcion}</p>
     `
     contenedorProductos.appendChild(tarjeta);
 })
